cdbeca56a58c7162d4f62509fc004d153d705df0
thomasrockhu <tom@fundersclub.com>
2017-06-27 13:34:39 -0400

    Use NotImplementedError

--- allauth/socialaccount/providers/intercom/views.py
+++ allauth/socialaccount/providers/intercom/views.py
@@ -15,30 +15,27 @@ class IntercomOAuth2Adapter(OAuth2Adapter):
 
     def complete_login(self, request, app, token, **kwargs):
         # This is a hack until intercom lets us know the profile url to use
-        try:
-            response = {}
+        response = {}
 
-            from allauth.socialaccount.models import SocialLogin, SocialAccount
+        from allauth.socialaccount.models import SocialLogin, SocialAccount
 
-            raise Exception
-            adapter = get_adapter(request)
-            uid = self.extract_uid(response)
-            extra_data = self.extract_extra_data(response)
-            common_fields = self.extract_common_fields(response)
-            socialaccount = SocialAccount(extra_data=extra_data,
-                                          uid=uid,
-                                          provider=self.id)
-            email_addresses = self.extract_email_addresses(response)
-            self.cleanup_email_addresses(common_fields.get('email'),
-                                         email_addresses)
-            sociallogin = SocialLogin(account=socialaccount,
-                                      email_addresses=email_addresses)
-            user = sociallogin.user = adapter.new_user(request, sociallogin)
-            user.set_unusable_password()
-            adapter.populate_user(request, sociallogin, common_fields)
-            return sociallogin
-        except Exception as e:
-            raise e
+        adapter = get_adapter(request)
+        uid = self.extract_uid(response)
+        extra_data = self.extract_extra_data(response)
+        common_fields = self.extract_common_fields(response)
+        socialaccount = SocialAccount(extra_data=extra_data,
+                                      uid=uid,
+                                      provider=self.id)
+        raise NotImplementedError
+        email_addresses = self.extract_email_addresses(response)
+        self.cleanup_email_addresses(common_fields.get('email'),
+                                     email_addresses)
+        sociallogin = SocialLogin(account=socialaccount,
+                                  email_addresses=email_addresses)
+        user = sociallogin.user = adapter.new_user(request, sociallogin)
+        user.set_unusable_password()
+        adapter.populate_user(request, sociallogin, common_fields)
+        return sociallogin
 
 oauth2_login = OAuth2LoginView.adapter_view(IntercomOAuth2Adapter)
 oauth2_callback = OAuth2CallbackView.adapter_view(IntercomOAuth2Adapter)
