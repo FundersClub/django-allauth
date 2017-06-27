8aa99f1bf5e4fe5f96a72f7ca39088b5db181bb1
thomasrockhu <tom@fundersclub.com>
2017-06-27 13:05:37 -0400

    try to figure out where the stuff is breaking

--- allauth/socialaccount/providers/intercom/views.py
+++ allauth/socialaccount/providers/intercom/views.py
@@ -4,6 +4,7 @@ from allauth.socialaccount.providers.oauth2.views import (
     OAuth2LoginView,
 )
 
+from ..adapter import get_adapter
 from .provider import IntercomProvider
 
 
@@ -15,7 +16,26 @@ class IntercomOAuth2Adapter(OAuth2Adapter):
     def complete_login(self, request, app, token, **kwargs):
         # This is a hack until intercom lets us know the profile url to use
         try:
-            return self.get_provider().sociallogin_from_response(request, {})
+            response = {}
+
+            from allauth.socialaccount.models import SocialLogin, SocialAccount
+
+            adapter = get_adapter(request)
+            uid = self.extract_uid(response)
+            extra_data = self.extract_extra_data(response)
+            common_fields = self.extract_common_fields(response)
+            socialaccount = SocialAccount(extra_data=extra_data,
+                                          uid=uid,
+                                          provider=self.id)
+            email_addresses = self.extract_email_addresses(response)
+            self.cleanup_email_addresses(common_fields.get('email'),
+                                         email_addresses)
+            sociallogin = SocialLogin(account=socialaccount,
+                                      email_addresses=email_addresses)
+            user = sociallogin.user = adapter.new_user(request, sociallogin)
+            user.set_unusable_password()
+            adapter.populate_user(request, sociallogin, common_fields)
+            return sociallogin
         except Exception as e:
             raise e
 
