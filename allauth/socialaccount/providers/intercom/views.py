2beded0c7499398ce6752a1ae958a8b64b36999b
thomasrockhu <tom@fundersclub.com>
2017-06-27 14:08:09 -0400

    Add the NotImplementedError now

--- allauth/socialaccount/providers/intercom/views.py
+++ allauth/socialaccount/providers/intercom/views.py
@@ -4,6 +4,7 @@ from allauth.socialaccount.providers.oauth2.views import (
     OAuth2LoginView,
 )
 
+from ...adapter import get_adapter
 from .provider import IntercomProvider
 
 
@@ -12,6 +13,29 @@ class IntercomOAuth2Adapter(OAuth2Adapter):
     access_token_url = 'https://api.intercom.io/auth/eagle/token'
     authorize_url = 'https://app.intercom.io/oauth'
 
+    def complete_login(self, request, app, token, **kwargs):
+        # This is a hack until intercom lets us know the profile url to use
+        raise NotImplementedError
+        response = {}
+
+        from allauth.socialaccount.models import SocialLogin, SocialAccount
+
+        adapter = get_adapter(request)
+        uid = self.extract_uid(response)
+        extra_data = self.extract_extra_data(response)
+        common_fields = self.extract_common_fields(response)
+        socialaccount = SocialAccount(extra_data=extra_data,
+                                      uid=uid,
+                                      provider=self.id)
+        email_addresses = self.extract_email_addresses(response)
+        self.cleanup_email_addresses(common_fields.get('email'),
+                                     email_addresses)
+        sociallogin = SocialLogin(account=socialaccount,
+                                  email_addresses=email_addresses)
+        user = sociallogin.user = adapter.new_user(request, sociallogin)
+        user.set_unusable_password()
+        adapter.populate_user(request, sociallogin, common_fields)
+        return sociallogin
 
 oauth2_login = OAuth2LoginView.adapter_view(IntercomOAuth2Adapter)
 oauth2_callback = OAuth2CallbackView.adapter_view(IntercomOAuth2Adapter)
